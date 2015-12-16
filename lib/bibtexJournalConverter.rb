c68a652a2b78746ce37116ef108107676535210d
Anurag Priyam <anurag08priyam@gmail.com>
2015-12-16 17:20:27 +0530

    Improvements.
    1. Rename --removeDots to --remove-dots and make it work.
    2. Speed up conversion drmatically!
    
    close #3
    
    Signed-off-by: Anurag Priyam <anurag08priyam@gmail.com>

--- lib/bibtexJournalConverter.rb
+++ lib/bibtexJournalConverter.rb
@@ -1,4 +1,3 @@
-
 require 'bibtex'
 require 'ruby-progressbar'
 
@@ -29,7 +28,6 @@ class Bibtexjournalconverter
   def run
     STDERR.puts "Will process: #{files}, with the following options: #{options}.\n"
 
-
     ## read conversion table
     longToShort = Hash.new
     options[:abbreviation_files].each do |abbreviations_file|
@@ -37,30 +35,22 @@ class Bibtexjournalconverter
       convertfile.each_line do |line|
         next if line.match(/^#/)
         long, short, extra = line.split(" = ")
-        short = short.gsub('.', '') if options[:removeDots]
         raise IOError("Too many '=' signs in: " + line) unless extra.nil?
+        short = short.gsub('.', '') if options[:remove_dots]
         longToShort[long] = short.chomp
-      end      
+      end
       STDERR.puts "Total #{longToShort.size} long/short title pairs after reading #{abbreviations_file}"
     end
 
-    ## do it baby 
+    ## do it baby
     bib = BibTeX.open(files[0])
-    progressbar = ProgressBar.create(:title => "Journals", :total => longToShort.size, :format=> "|%B| %E", :output => STDERR)
+    progressbar = ProgressBar.create(:title => "Journals", :total => longToShort.size,
+                                     :format=> "|%B| %E", :output => STDERR)
     journalConversion = (options[:shorten] == true) ? longToShort : longToShort.invert
-    journalConversion.each_key do |oldname|
-      progressbar.increment
-      #STDERR.puts "converting #{oldname}\t to #{journalConversion[oldname]}"
-      bib.unify(:journal, Regexp.new('^'+oldname+'$', 'i'), journalConversion[oldname])
-    end
 
-    ## removing any extra '.' from journals that were already short
-    if options[:shorten] == true
-      bib.each do |bibitem|
-        next unless bibitem.respond_to?('journal')
-        next if bibitem.journal.nil?
-        bibitem.journal.gsub!(/\./, '')
-      end
+    bib.each do |entry|
+      entry.journal = journalConversion[entry.journal] || entry.journal
+      progressbar.increment
     end
 
     STDERR.puts 'Done.'
